a26536d9d07d76172b3d8813c9291e62173c6c36
jakartasipirok <kevinroni2002@gmail.com>
2021-11-24 21:35:14 +0700

    bersih2

--- main.py
+++ main.py
@@ -24,14 +24,10 @@ def cyk(w,grammar):
                         if len(terminal) == 2 :
                             if(terminal[0] in tab[j][l]) and (terminal[1] in tab[l+1][i]): #ketemu terminal
                                 tab[j][i].add(rule[0])
-    # print(tab)
-    # print(tab[0][n-1])
     if "S0" in tab[0][n-1] :
         print("Accepted")
-        #return True
     else :
         print("Syntax Error")
-        #return False
         
 if len(sys.argv) > 1:
     	modelPath = str(sys.argv[1])

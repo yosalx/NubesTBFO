26810968fecd172fb0c7709b88b1bd890bdeda1d
jakartasipirok <kevinroni2002@gmail.com>
2021-11-24 16:13:13 +0700

    benerin cyk

--- cfg2cnf.py
+++ cfg2cnf.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		modelPath = str(sys.argv[1])
 	else:
-		modelPath = 'grammar.txt'
+		modelPath = 'model.txt'
 	
 	K, V, Productions = loadModel( modelPath )
 
@@ -189,32 +189,35 @@ def prodToDict(productions):
  
 x = prodToDict(Productions)
 
-def cyk(w,cnfGram):
+def cyk(w,grammar):
     n = len(w)
-    dp = [[set([]) for i in range(n)] for j in range(n)]
 
-    for i in range(n):
-        for var in cnfGram.items():
-                for termin in var[1]:
-                    if len(termin) == 1 and termin[0] == w[i]:
-                        dp[i][i].add(var[0])
+	#init table
+    tab = [[set([]) for j in range(n)] for i in range(n)]
 
-    for l in range(2,n+1):
-        for i in range (0,n-l+1):
-            j = i+l-1
-            for k in range (i,j):
-                for var in cnfGram.items():
-                    for prod in var[1] :
-                        if len(prod) == 2 :
-                            if(prod[0] in dp[i][k]) and (prod[1] in dp[k+1][j]):
-                                dp[i][j].add(var[0])
-    # print(dp)
-    # print(dp[0][n-1])
-    if "S0" in dp[0][n-1] :
-        print("Accepted Answer! :D")
+	#isi table
+    for j in range(0, n):
+        for rule in grammar.items():
+                for terminal in rule[1]:
+                    if len(terminal) == 1 and terminal[0] == w[j]: #ketemu terminal
+                        tab[j][j].add(rule[0])
+
+    for k in range(2, n+1):
+        for j in range (0, n-k+1):
+            i = j+k-1
+            for l in range (j,i):
+                for rule in grammar.items():
+                    for terminal in rule[1] :
+                        if len(terminal) == 2 :
+                            if(terminal[0] in tab[j][l]) and (terminal[1] in tab[l+1][i]): #ketemu terminal
+                                tab[j][i].add(rule[0])
+    # print(tab)
+    # print(tab[0][n-1])
+    if "S0" in tab[0][n-1] :
+        print("Accepted")
         #return True
     else :
-        print("Syntax Error :(")
+        print("Syntax Error")
         #return False
         
 import basedSlice
